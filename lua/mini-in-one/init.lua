6722acc0e75fc12af62308d9cf921b83640deb4e
Ilgar Gamidov <braam76.gamidov@yandex.com>
2026-06-08 18:56:49 +0300

    Annotated everything with types again (hope it works)

--- lua/mini-in-one/init.lua
+++ lua/mini-in-one/init.lua
@@ -1,51 +1,5 @@
+---@module "mini-in-one.types"
 local M = {}
-
----@class MiniInOneConfig
----@field ai? table
----@field align? table
----@field comment? table
----@field completion? table
----@field keymap? table
----@field move? table
----@field operators? table
----@field pairs? table
----@field snippets? table
----@field splitjoin? table
----@field surround? table
----@field basics? table
----@field bracketed? table
----@field bufremove? table
----@field clue? table
----@field cmdline? table
----@field deps? table
----@field diff? table
----@field extra? table
----@field files? table
----@field git? table
----@field input? table
----@field jump? table
----@field jump2d? table
----@field misc? table
----@field pick? table
----@field sessions? table
----@field visits? table
----@field animate? table
----@field base16? table
----@field colors? table
----@field cursorword? table
----@field hipatterns? table
----@field hues? table
----@field icons? table
----@field indentscope? table
----@field map? table
----@field notify? table
----@field starter? table
----@field statusline? table
----@field tabline? table
----@field trailspace? table
----@field doc? table
----@field fuzzy? table
----@field test? table
 
 --- Sets up mini.nvim modules in one setup call
 ---
@@ -59,8 +13,8 @@ local M = {}
 ---   },
 --- })
 --- ```
---- @param config MiniInOneConfig
---- @return table M The module table with loaded mini modules
+--- @param config MiniInOneConfig Configuration table where keys are mini module names
+--- @return MiniInOne
 function M.setup(config)
   for module_name, opts in pairs(config) do
     -- Try to load the module
